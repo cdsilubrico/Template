0767d51f8165199c55b17bd92c2826eaafff971b
Clowie Silubrico <clowiesilubrico@gmail.com>
2022-07-27 23:54:11 +0800

    Update main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import regex.Regex;
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println(Regex.onlyAcceptsNumericalValues("1231.01ASDA!@"));
 
     }
 }
